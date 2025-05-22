83528e0606e5833ca63837f3bffc3daac26076cd
darling7429 <53552021+darling7429@users.noreply.github.com>
2025-05-22 11:34:46 -0500

    created a navbar for restro

---
+++ index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img className="reslogo" src="https://res.cloudinary.com/dutdah0l9/image/upload/v1720058694/Swiggy_logo_bml6he.png" alt="res-logo" />
+      </div>
+      <div className="nav_items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Menu</li>
+          <li>Contact Us</li>
+          <button className="nav_button1">
+            Get The App     <i class="fa-solid fa-square-arrow-up-right"></i>
+          </button>
+          <button className="nav_button2">Signin</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return <div className="body"></div>;
+};
+
+const Footer = () => {
+  return <div></div>;
+};
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
